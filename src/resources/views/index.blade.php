4f7745fed0d572a76adf52034090ff02c5ef271d
Yacine REZGUI <yacine.rezgui@gmail.com>
2021-08-20 19:51:24 +0100

    Update index.blade.php

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,7 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Live-tinker</title>
     <link rel="stylesheet" href="/vendor/live-tinker/css/app.css">
 </head>
